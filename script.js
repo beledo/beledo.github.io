30d8a234e9aff838398a430a6e9a8e38a46917e5
Octavio Beledo <octaviobeledo13@gmail.com>
2026-03-01 12:32:04 +0100

    path of script js

--- script.js
+++ script.js
@@ -35,3 +35,10 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("nav");
+
+hamburger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
